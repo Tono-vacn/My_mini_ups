b9bb9d29b4e66f15f851ed305a5d8c66235cf459
Tono-vacn <yuchen.jiang@duke.edu>
2024-04-22 21:52:42 -0400

    fix webapp

--- my_ups/myups/admin.py
+++ my_ups/myups/admin.py
@@ -1,1 +1,7 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(World)
+admin.site.register(Account_tmp)
+admin.site.register(Truck_Set)
+admin.site.register(Package_tmp)
